0b73506b9f08c12f39d2536f5493e9869abd2963
rajatrv <rajatrv@gmail.com>
2020-09-25 17:33:10 -0400

    Create scoutsuite.sh

---
+++ scoutsuite.sh
@@ -0,0 +1,21 @@
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+python3 --version
+yum install python3
+
+python3 -m pip install --user virtualenv
+python3 -m pip install --user --upgrade pip
+python3 -m pip --version
+
+python3 -m venv env
+source env/bin/activate
+
+yum install git
+git clone https://github.com/nccgroup/ScoutSuite
+
+cd ScoutSuite/
+pip install -r requirements.txt
+
+python scout.py aws
+cd scoutsuite-report
+aws s3 sync . s3:// <>
